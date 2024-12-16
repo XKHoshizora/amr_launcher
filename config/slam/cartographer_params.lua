23a39bf8e6c1302baf107e24183848afaba3b30c
XKHoshizora <hoshizoranihon@gmail.com>
2024-12-16 22:25:02 +0900

    Update params

--- config/slam/cartographer_params.lua
+++ config/slam/cartographer_params.lua
@@ -21,14 +21,13 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "imu_link",
+  tracking_frame = "base_link",
   published_frame = "base_link",
   odom_frame = "odom",
   provide_odom_frame = true,
   publish_frame_projected_to_2d = false,
   use_pose_extrapolator = true,
   use_odometry = false,
-  use_imu = false,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 0,
@@ -42,11 +41,11 @@ options = {
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
   fixed_frame_pose_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.,
+  imu_sampling_ratio = 0.,
   landmarks_sampling_ratio = 1.,
 }
 
--- TRAJECTORY_BUILDER_2D.use_imu_data = false
+TRAJECTORY_BUILDER_2D.use_imu_data = false
 
 -- 2D SLAM和后台线程设置
 MAP_BUILDER.use_trajectory_builder_2d = true
