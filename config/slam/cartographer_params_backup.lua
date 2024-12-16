861aeeddde4b40a63dcf00662d0f3fa5a26f5e6b
XKHoshizora <hoshizoranihon@gmail.com>
2024-12-16 22:16:28 +0900

    Update params

--- config/slam/cartographer_params_backup.lua
+++ config/slam/cartographer_params_backup.lua
@@ -20,26 +20,30 @@ include "trajectory_builder.lua"
 options = {
     map_builder = MAP_BUILDER,
     trajectory_builder = TRAJECTORY_BUILDER,
-
-    -- 坐标系配置
-    map_frame = "map",  -- 地图坐标系
-    tracking_frame = "base_link",  -- 使用IMU数据时设置为"imu_link"
-    tracking_frame = "imu_link",  -- 使用IMU数据时设置为"imu_link"
-    published_frame = "odom",  -- 发布的里程计坐标系
-    odom_frame = "odom",  -- 发布的里程计坐标系
-    provide_odom_frame = false,  -- 已有外部里程计数据
-
-    -- 传感器使能配置
-    use_odometry = true,  -- 使用里程计数据
-    use_imu_data = true  -- 启用IMU数据
-    use_nav_sat = false,  -- 使用GPS数据
-    use_landmarks = false,  -- 使用地标数据
-
-    -- 传感器话题配置
-    num_laser_scans = 1,  -- 使用一个单独的激光雷达
-    num_multi_echo_laser_scans = 0,
+    map_frame = "map",
+    tracking_frame = "base_link",
+    published_frame = "base_link",
+    odom_frame = "odom",
+    provide_odom_frame = true,
+    publish_frame_projected_to_2d = false,
+    use_pose_extrapolator = true,
+    use_odometry = false,
+    use_nav_sat = false,
+    use_landmarks = false,
+    num_laser_scans = 0,
+    num_multi_echo_laser_scans = 1,
+    num_subdivisions_per_laser_scan = 10,
     num_point_clouds = 0,
-}
+    lookup_transform_timeout_sec = 0.2,
+    submap_publish_period_sec = 0.3,
+    pose_publish_period_sec = 5e-3,
+    trajectory_publish_period_sec = 30e-3,
+    rangefinder_sampling_ratio = 1.,
+    odometry_sampling_ratio = 1.,
+    fixed_frame_pose_sampling_ratio = 1.,
+    imu_sampling_ratio = 1.,
+    landmarks_sampling_ratio = 1.,
+  }
 
 -- 2D SLAM和后台线程设置
 MAP_BUILDER.use_trajectory_builder_2d = true
